444be20e6275909f98f4efd23d658c0ad4211ce2
Kevin Jahns <kevin.jahns@rwth-aachen.de>
2015-08-24 05:29:17 +0200

    added openshift support

--- l2p-proxy.js
+++ l2p-proxy.js
@@ -1,6 +1,6 @@
 // Heroku defines the environment variable PORT, and requires the binding address to be 0.0.0.0 
-var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
-var port = process.env.PORT || 8686;
+var host = process.env.OPENSHIFT_NODEJS_IP || (process.env.PORT ? '0.0.0.0' : '127.0.0.1')
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8686;
  
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
